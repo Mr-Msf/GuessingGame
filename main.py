630a3d76f7e5b77f8496bf3695d96e3af07255c2
MrMsf <mikaelsharifyfunk@gmail.com>
2020-11-15 01:53:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+  print("Welcome to the guessing game.")
+  print("The object of the game is to guess a number between 1 and 1000 within 5 tries.")
+
+if __name__ == "__main__":
+  main()
